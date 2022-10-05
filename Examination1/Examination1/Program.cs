6c29499330716d8f3ae32bbfbe0a060803705b69
rob-katt <gabbe.carlin@gmail.com>
2022-10-05 09:34:14 +0200

    Uploading files

---
+++ Examination1/Examination1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+while(true)
+{
+    
+    string Menu = Console.ReadLine();
+
+    // meny för cirkelns matte
+    if (Menu == "1")
+    {
+        Console.WriteLine("1: Calculate Area");
+        Console.WriteLine("2: Calculate Circumference");
+        Console.WriteLine("0: Back");
+        Menu = Console.ReadLine();
+        if (Menu == "1")
+        {
+            Console.Write("Input Radius: ");
+            int Radius = int.Parse(Console.ReadLine());
+            Console.WriteLine("Area= "+(Radius*2*Math.PI));
+        }
+    }
+
+
+}
