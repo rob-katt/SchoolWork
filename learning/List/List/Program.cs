1f4a698343102b8d68eeb909c63937ccf31c649a
rob-katt <gabbe.carlin@gmail.com>
2022-10-26 12:26:10 +0200

    list

---
+++ learning/List/List/Program.cs
@@ -0,0 +1,18 @@
+﻿string sentence = Console.ReadLine();
+string[] sentencelist = sentence.Split(" ");
+
+int longWordCount = 0;
+int wordcount = 0;
+foreach (string word in sentencelist)
+{
+    if (word.Count() > 5)
+    {
+        longWordCount++;
+    }
+
+
+    wordcount=wordcount+1;
+    Console.WriteLine(word);
+}
+Console.WriteLine(wordcount);
+Console.WriteLine(longWordCount);
