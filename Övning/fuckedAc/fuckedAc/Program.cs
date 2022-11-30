27e809b0f0176f55d1230aec8e9af9a33e62a95f
rob-katt <gabbe.carlin@gmail.com>
2022-11-30 10:29:57 +0100

    AC
    what the fuck is wrong with this ac

---
+++ Övning/fuckedAc/fuckedAc/Program.cs
@@ -0,0 +1,18 @@
+﻿int temperature = 20;
+while (true)
+{
+    Console.WriteLine("Temperature: " + temperature);
+    Console.WriteLine("# to turn off the AC");
+    string answer = Console.ReadLine();
+
+    if (answer == "#") { Environment.Exit(0); }
+
+    Console.WriteLine("Insert temperature");
+    temperature = int.Parse(Console.ReadLine());
+    if (temperature > 25) { temperature = 20; }
+    else if (temperature < 17) { temperature = 20; }
+
+    
+
+
+}
