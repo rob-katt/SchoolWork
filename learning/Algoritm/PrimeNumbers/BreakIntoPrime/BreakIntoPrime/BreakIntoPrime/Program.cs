63b8600681d83894b92a9411c8ac401ece8b0740
rob-katt <gabbe.carlin@gmail.com>
2023-03-02 20:49:52 +0100

    yes

---
+++ learning/Algoritm/PrimeNumbers/BreakIntoPrime/BreakIntoPrime/BreakIntoPrime/Program.cs
@@ -0,0 +1,35 @@
+﻿static string PrimeFactorizing(long number)
+{
+    string primeNumbers = "";
+    int min = 2;
+    long max = number;
+
+    while (min < max)
+    {
+        if (max % min == 0)
+        {
+            primeNumbers+= min + "*";
+            max = max/min;
+        }
+        else
+        {
+            min++;
+        }
+    }
+
+    primeNumbers += max;
+    if (primeNumbers == max.ToString())
+    {
+        return (number + " is a prime number");
+    }
+    else
+    {
+        return primeNumbers;
+    }
+
+}
+
+long input = long.Parse(Console.ReadLine());
+
+Console.WriteLine(PrimeFactorizing(input));
+//long my beloved
